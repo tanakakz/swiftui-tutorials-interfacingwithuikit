e0803f728e631242f42aef5a7754e6599eeab7e7
tanakakz <tanakakz.feb.23@gmail.com>
2020-01-30 08:00:48 +0900

    coodinator and controller are connected in both directions

--- Landmarks/Landmarks/Home/PageViewController.swift
+++ Landmarks/Landmarks/Home/PageViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 
 struct PageViewController: UIViewControllerRepresentable {
     var controllers: [UIViewController]
+    @Binding var currentPage: Int
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -20,17 +21,21 @@ struct PageViewController: UIViewControllerRepresentable {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
-        pageViewController.dataSource = context.coordinator // 操作時のデータの扱い方をビューの制御役に渡す
+        // 以下２行の代入により、双方向のバインディングを実現する。
+        // 具体的には、データが変わればビュー側に反映されるし、逆に、ビュー側の操作もデータに反映される。
+        pageViewController.dataSource = context.coordinator // データ操作に伴うビューへの反映を制御役(コントローラー)にお願いする
+        pageViewController.delegate = context.coordinator // ビュー操作に伴うデータへの反映を調整役(コーディネイター)に委任する
         
         return pageViewController
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [controllers[0]], direction: .forward, animated: true)
+            [controllers[currentPage]], direction: .forward, animated: true)
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    // ドーモ、コントローラー=サン コーディネイターです
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         
         init(_ pageViewController: PageViewController) {
@@ -61,6 +66,20 @@ struct PageViewController: UIViewControllerRepresentable {
                 return parent.controllers.first
             }
             return parent.controllers[index + 1]
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            didFinishAnimating finished: Bool,
+            previousViewControllers: [UIViewController],
+            transitionCompleted completed: Bool
+        ) {
+            if completed,
+                let visibleViewController = pageViewController.viewControllers?.first,
+                let index = parent.controllers.firstIndex(of: visibleViewController)
+            {
+                parent.currentPage = index
+            }
         }
     }
 }
