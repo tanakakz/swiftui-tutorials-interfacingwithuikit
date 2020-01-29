32b8b453a20175a91a26a194aa51c34e811e46e5
tanakakz <tanakakz.feb.23@gmail.com>
2020-01-29 10:55:41 +0900

    add page controller and view

---
+++ Landmarks/Landmarks/Home/PageView.swift
@@ -0,0 +1,28 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by tkz on 2020/01/29.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var viewControllers: [UIHostingController<Page>]
+    
+    init(_ views: [Page]) {
+        self.viewControllers = views.map { UIHostingController(rootView: $0) }
+    }
+    
+    var body: some View {
+        PageViewController(controllers: viewControllers)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(features.map {FeatureCard(landmark: $0) })
+            .aspectRatio(3/2, contentMode: .fit)
+    }
+}
